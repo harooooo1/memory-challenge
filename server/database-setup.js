a18078c26a0670561ebdc39db003ea900092272f
harooooo1 <piricharun@gmail.com>
2020-08-03 16:15:12 +0200

    creating database tables

--- server/database-setup.js
+++ server/database-setup.js
@@ -14,22 +14,79 @@ const database = new Sequelize("memory_challenge", "root", "adminadmin", {
 });
 
 // Model definition
-/* const Hello = database.define("users", {
-  name: Sequelize.STRING,
+ const User = database.define("User", {
+  id: Sequelize.INTEGER,
+  username: Sequelize.STRING,
+  firstname: Sequelize.STRING,
+  lastname: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  totalwins: Sequelize.INTEGER,
+  banned: Sequelize.TINYINT,
   foo: Sequelize.JSON,
 });
 
-const Hello2 = database.define("hello_2", {
-  name: Sequelize.STRING,
-  bar: Sequelize.JSON,
-}); */
+const Game = database.define("Game", {
+  id: Sequelize.INTEGER,
+  //FK
+  hostname: {
+    type: Sequelize.STRING,
+    references: {
+       model: 'User', // from which table the FK is pulled from
+       key: 'username', // which column
+    }
+  },
+  gamename: Sequelize.STRING,
+  current_players: Sequelize.INTEGER, //current players
+  max_players: Sequelize.INTEGER,     //max players
+  state: Sequelize.STRING,
+  start_time: Sequelize.STRING,
+  winner: Sequelize.STRING, // data about who won the game, need to find a way to return this value to this table after a game ends
+
+}); 
+
+const Player = database.define("Player", {
+  id: {
+    type: Sequelize.INTEGER,
+    references: {
+       model: 'User', // from which table the FK is pulled from
+       key: 'id', // which column
+    }
+  },
+
+
+  game_id: {
+    type: Sequelize.INTEGER,
+    references: {
+       model: 'Game', // from which table the FK is pulled from
+       key: 'id', // which column
+    }
+  },
+  username: {
+    type: Sequelize.STRING,
+    references: {
+       model: 'User', // from which table the FK is pulled from
+       key: 'username', // which column
+    }
+  },
+  player_number: Sequelize.INTEGER, //players number inside the game (example: player1, player2 etc....)
+  clicks: Sequelize.INTEGER,     // number of times you clicked on a card to reveal it
+  pairs: Sequelize.INTEGER,      // number of succesfully matched pairs
+
+}); 
+
 
 // establish a link between the two entities
 //Hello.hasOne(Hello2);
 //Hello2.belongsTo(Hello);
 
+User.hasMany(Game);
+Game.hasMany(Player);
+User.hasOne(Player);
+Player.belongsTo(User);
+
 // INIT DB ENTITY MODELS
-/*(async function () {
+(async function () {
   // Drop tables in order to avoid foreign key constraint issues
   if (FORCE_RECREATE_MODELS) {
     Hello2.drop(); // Hello has a foreign key referencing Hello
@@ -49,4 +106,3 @@ const Hello2 = database.define("hello_2", {
 module.exports.Hello = Hello;
 module.exports.Hello2 = Hello2;
 module.exports.database = database;
-*/
